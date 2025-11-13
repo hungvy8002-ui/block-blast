1335942b2e2ec5503c5d8d1610dddc2f8a98f5e5
hungvy8002-ui <hungvy8002@gmail.com>
2025-11-13 22:59:45 +0700

    Update checkBlocks.js

--- checkBlocks.js
+++ checkBlocks.js
@@ -1,39 +1,103 @@
-// checkBlocks.js
-export function onCheckBlocks(ctx) {
-    var t = ctx.view.block_map,
-        o = c.control.block_size,
-        e = !0,
-        n = [];
+o.prototype.onCheckBlocks = function() {
+    var t = this.view.block_map;        // mảng 2D [row][col] block
+    var o = c.control.block_size;       // kích thước block
+    var allFit = true;                  // flag kiểm tra toy_node chạm đúng vị trí
+    var n = [];                          // danh sách block trúng toy_node
 
-    ctx.fs_list = [];
-    ctx.bingo_i = [];
-    ctx.bingo_j = [];
+    this.fs_list = [];
+    this.bingo_i = [];                   // hàng bingo
+    this.bingo_j = [];                   // cột bingo
 
-    for (var i = 0; i < ctx.toy_node.children.length; i++) {
-        for (var r = !1, s = 0; s < t.length; s++)
-            for (var a = 0; a < t[0].length; a++) {
-                var h = t[s][a];
-                if (h.getOpacity() < 255) h.setOpacity(0);
-                h.setColor(h.ori_color_num);
-                if (e) {
-                    var l = h.node.convertToNodeSpaceAR(
-                        ctx.toy_node.children[i].convertToWorldSpaceAR(cc.v2(0, 0))
+    // --- 1. Lọc các block trúng toy_node ---
+    for (var i = 0; i < this.toy_node.children.length; i++) {
+        var blockHit = false;
+        for (var row = 0; row < t.length; row++) {
+            for (var col = 0; col < t[0].length; col++) {
+                var block = t[row][col];
+                if (block.getOpacity() < 255) block.setOpacity(0);
+                block.setColor(block.ori_color_num);
+                
+                if (allFit) {
+                    var pos = block.node.convertToNodeSpaceAR(
+                        this.toy_node.children[i].convertToWorldSpaceAR(cc.v2(0, 0))
                     );
-                    if (l.x >= -o / 2 && l.x < o / 2 && l.y >= -o / 2 && l.y < o / 2) {
-                        if (!h.getOpacity()) r = !0;
-                        n.push(h);
+                    if (pos.x >= -o/2 && pos.x < o/2 && pos.y >= -o/2 && pos.y < o/2) {
+                        if (!block.getOpacity()) blockHit = true;
+                        n.push(block);
                     }
                 }
             }
-        if (!r) e = !1;
+        }
+        if (!blockHit) allFit = false;
     }
 
-    if (e) {
-        var p = [],
-            u = [];
-        for (s = 0; s < n.length; s++) {
-            var h = n[s];
-            if (!h.getOpacity()) {
+    if (!allFit) return; // nếu không tất cả toy_node đúng vị trí thì thôi
+
+    // --- 2. Chuẩn bị các cột/hàng đã xử lý ---
+    var processedRows = [];
+    var processedCols = [];
+    
+    // --- 3. Xử lý từng block trong danh sách n ---
+    for (var idx = 0; idx < n.length; idx++) {
+        var block = n[idx];
+
+        // đánh dấu block chưa xử lý bingo
+        if (block.processed === undefined) block.processed = false;
+
+        var row = block.i;
+        var col = block.j;
+
+        // --- Row bingo ---
+        if (processedRows.indexOf(row) === -1) {
+            processedRows.push(row);
+            var rowBingo = true;
+            for (var c = 0; c < t[row].length; c++) {
+                if (t[row][c].getOpacity() <= 0) {
+                    rowBingo = false;
+                    break;
+                }
+            }
+            if (rowBingo) {
+                this.bingo_i.push(row);
+                for (var c = 0; c < t[row].length; c++) {
+                    var b = t[row][c];
+                    if (!b.processed) {
+                        b.setColor(this.color_idx);
+                        b.setOpacity(127);
+                        b.addGlow(this.color_idx, 1.5);
+                        this.fs_list.push(b);
+                        b.processed = true;
+                    }
+                }
+            }
+        }
+
+        // --- Column bingo ---
+        if (processedCols.indexOf(col) === -1) {
+            processedCols.push(col);
+            var colBingo = true;
+            for (var r = 0; r < t.length; r++) {
+                if (t[r][col].getOpacity() <= 0) {
+                    colBingo = false;
+                    break;
+                }
+            }
+            if (colBingo) {
+                this.bingo_j.push(col);
+                for (var r = 0; r < t.length; r++) {
+                    var b = t[r][col];
+                    if (!b.processed) {
+                        b.setColor(this.color_idx);
+                        b.setOpacity(127);
+                        b.addGlow(this.color_idx, 1.5);
+                        this.fs_list.push(b);
+                        b.processed = true;
+                    }
+                }
+            }
+        }
+    }
+}
                 h.setColor(ctx.color_idx);
                 h.setOpacity(127);
                 ctx.fs_list.push(h);
